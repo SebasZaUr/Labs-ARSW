c717d216a134607486072e8edd2693c08cca8725
Medina95 <carolina.medina-a@mail.escuelaing.edu.co>
2024-02-14 23:12:22 -0500

    Adelanto Consumidor y Productor :D

--- Lab-3-ConcurrentProgramming_Syncrhronization_DeadLocks_ThsSuspension/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ Lab-3-ConcurrentProgramming_Syncrhronization_DeadLocks_ThsSuspension/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -7,6 +7,7 @@ package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
 import java.util.Random;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,13 +17,13 @@ import java.util.logging.Logger;
  */
 public class Producer extends Thread {
 
-    private Queue<Integer> queue = null;
+    private LinkedBlockingQueue<Integer> queue = null;
 
     private int dataSeed = 0;
     private Random rand=null;
     private final long stockLimit;
 
-    public Producer(Queue<Integer> queue,long stockLimit) {
+    public Producer(LinkedBlockingQueue<Integer> queue, long stockLimit) {
         this.queue = queue;
         rand = new Random(System.currentTimeMillis());
         this.stockLimit=stockLimit;
@@ -31,15 +32,20 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
+            synchronized (queue) {
 
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                while (queue.remainingCapacity() == 0){
+                    try {
+                        queue.wait();
+                    }catch (InterruptedException ex) {
+                        Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
+                queue.notify();
+
             }
 
         }
